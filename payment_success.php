8cadcbadebe2f4b58a3975aa24e56a30ed03392f
skavinsankar <127394298+skavinsankar@users.noreply.github.com>
2025-04-22 22:55:03 +0530

    Add files via upload

---
+++ payment_success.php
@@ -0,0 +1,57 @@
+<?php
+include "header.php"; 
+
+
+echo "<div class='payment-success'>";
+echo "<h1>Payment Success</h1>";
+echo "<p>Thank you for your payment! Your transaction has been processed successfully.</p>";
+echo "</div>";
+
+include "newslettter.php"; 
+include "footer.php"; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
